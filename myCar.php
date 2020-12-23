a406298716f6a695131df1524f44957b953ce0c2
Richell <kredoteacher.richel@gmail.com>
2020-12-23 15:14:27 +0800

    new uploads

--- myCar.php
+++ myCar.php
@@ -10,10 +10,10 @@
     echo "MODEL: " .$car->get_model(). "<br>";
     echo "COLOR: " .$car->get_color(). "<br>";
 
-    // echo "<br>";
-    // echo "MAKE (calling of public property): " .$car->make. "<br>";
-    // echo "MODEL (calling of public property): " .$car->model. "<br>";
-    // echo "COLOR (calling of public property): " .$car->color. "<br>";
+    echo "<br>";
+    echo "MAKE (calling of public property): " .$car->make. "<br>";
+    echo "MODEL (calling of public property): " .$car->model. "<br>";
+    echo "COLOR (calling of public property): " .$car->color. "<br>";
 
 ?>
 
